40c4caab8e7ae1108122c490f5d0b0aa807e7ebc
Chris Kees <613654+cekees@users.noreply.github.com>
2025-05-13 18:33:06 -0500

    define cmake policy version 3.5

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,7 @@ cmake -H. \
     -Bbuild -GNinja \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
     -DCMAKE_INSTALL_PREFIX=$PREFIX
 cmake --build build/ -- -j${CPU_COUNT}
 cmake --build build/ -- install
