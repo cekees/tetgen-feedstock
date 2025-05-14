cbabf6ee36ddc9d77e14d16ddad1305b3f89e759
Chris Kees <613654+cekees@users.noreply.github.com>
2025-05-13 20:56:34 -0500

    Update bld.bat with cmake policy version 3.5

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,6 +9,7 @@ cmake -G "NMake Makefiles" ^
     -D CMAKE_BUILD_TYPE=Release ^
     -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -D CMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ^
     %SRC_DIR%
 if errorlevel 1 exit 1
 
